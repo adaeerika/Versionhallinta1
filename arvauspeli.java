d75ffab6a5d2dab55a50dcd05374deb76a0a37ab
adaeerika <ada23003@student.hamk.fi>
2024-12-09 17:44:00 +0200

    arvoja sunmuita

--- arvauspeli.java
+++ arvauspeli.java
@@ -1,5 +1,13 @@
 package Versionhallinta;
+import java.util.Scanner;
 
 public class arvauspeli {
+    public static void main(String[] args) {
+        String nimi = "Ada";
+        int arvauksia = 0;
+        int maxYritykset = 5;
+        Scanner scanner = new Scanner(System.in);
 
+        
+    }
 }
