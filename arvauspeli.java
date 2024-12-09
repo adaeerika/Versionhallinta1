2c9fd2bae5e117e10b9bbd3e2e9769e8448fba50
adaeerika <ada23003@student.hamk.fi>
2024-12-09 17:59:14 +0200

    if ja else loopit

--- arvauspeli.java
+++ arvauspeli.java
@@ -12,6 +12,22 @@ public class arvauspeli {
             System.out.print("Arvaa nimi (tai kirjoita 'loppu' lopettaaksesi pelin): ");
             String arvaus = scanner.nextLine();
             arvauksia++;
+
+            if (arvaus.equalsIgnoreCase("loppu")) {
+                System.out.println("Peli loppui. Et arvannut nimeä.");
+                break;
+            }
+            else if (arvaus.equals(nimi)) {
+                System.out.println("Onneksi olkoon! Arvasit oikein " + arvauksia + ". yrityksellä.");
+                break;
+            }
+            else if (arvauksia == maxYritykset) {
+                System.out.println("Yritykset loppuivat! Oikea nimi oli: " + nimi);
+            }
+            else {
+                System.out.println("Väärin! Yritä uudestaan.");
+            }
         }
+        scanner.close();
     }
 }
